c218c239e4be03b0c6617be84836969cd0198b82
Andrea Florio <andrea@opensuse.org>
2021-09-21 12:19:15 +0200

    Initial MQTT definitions

--- esp8266-hunter-sprinkler/include/global_config.h
+++ esp8266-hunter-sprinkler/include/global_config.h
@@ -14,4 +14,10 @@ extern const String device_hostname;
 #define SSID_AP    "WateringSystemAP"   // SSID of the AP created to configure WiFi
 #define PWD_AP     ""                   // Password of the AP created to configure WiFi
 
+//MQTT
+extern const char* mqtt_server;
+extern const int mqtt_port; //default is 1883
+extern const char *mqtt_user;
+extern const char *mqtt_pass;
+
 #endif
